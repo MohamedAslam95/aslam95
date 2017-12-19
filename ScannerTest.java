986b6d3cd671c62a1caecd6b560552c38ab07aca
Mohamed Aslam <hmaslamhndit@gmail.com>
2017-12-19 09:42:18 +0530

    changed: Add some commends in java

---
+++ ScannerTest.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ScannerTest
+{
+
+ public static void main(String[] arg)
+ {
+
+  Scanner input = new Scanner(System.in);
+
+//Test Command
+
+  System.out.print("Enter an Integer between 0 and 1000: ");
+  int num = input.nextInt();
+
+		int t=0; //test
+		t+=num%10; //5
+		num/=10;	//12
+		t+=num%10;	//7
+		num/=10;	//1
+		t+=num%10; //8
+			
+System.out.println("This Output is:"+t);
+
+ }
+}
